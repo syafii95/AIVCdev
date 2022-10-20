b7970fca8dc535d13d0cffc9dbb0cee5e6e64483
Syafi'i <muhd_syafii@topglove.com.my>
2022-10-20 14:15:28 +0800

    rename security dialog file

--- utils/securityDialog.py
+++ utils/securityDialog.py
@@ -11,7 +11,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Dialog(object):
+class Ui_SecurityDialog(object):
     def setupUi(self, Dialog):
         Dialog.setObjectName("Dialog")
         Dialog.resize(415, 123)
