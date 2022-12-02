fa616df3daa52953de0b929d5b174faa81a20030
Syafi'i <muhd_syafii@topglove.com.my>
2022-12-02 15:08:15 +0800

    manually copy pyqtgraph lib to the execute path

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os
+import shutil 
 import sys
 from cx_Freeze import setup, Executable
 
@@ -5,6 +7,23 @@ build_exe_options = {"packages": ["MvImport","pyqt5","tensorboard.summary","cv2"
 "astor","termcolor","opt_einsum","google.protobuf","tensorflow_estimator","matplotlib","os", "tensorflow", "numpy", "PIL"],\
  "include_files": ["core", "color_mask_threshold.txt", "data","classes.names", "Login.ini", "aivcMonitor","lib"],  \
  "excludes": ["matplotlib.tests", "numpy.random._examples"]}
+
+def copyFile():
+    paths = os.path.abspath(os.getcwd())
+    listfile = ["pyqtgraph","pyqtgraph-0.11.1.dist-info"]
+    for j in range (len(listfile)):
+        src = f'{paths}/{listfile[j]}'
+        libPath = f'{paths}/build/exe.win-amd64-3.6/lib/{listfile[j]}'
+        src = src.replace("\\", "/")
+        libPath = libPath.replace("\\", "/")
+        if not os.path.exists(libPath):
+            shutil.copytree(src, libPath) 
+            print(f'Path not exist. Create new one: {libPath}')
+        else:
+            print(f'Path exist. Replace with {libPath}')
+            shutil.rmtree(libPath)
+            shutil.copytree(src, libPath)
+
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
@@ -18,3 +37,5 @@ setup(  name = "AIVC",
         author = "Syafii",
         options = {"build_exe": build_exe_options},
         executables = [Executable("AIVC.py", base=base, icon='utils/icons/TG_icon.ico')])
+
+copyFile()
